d474c2dd91becb721041b3f3ae3a88c633f329b7
Lauren Goss <laurenlgoss98@gmail.com>
2021-11-13 13:46:47 -0500

    got user's personal profile page working

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-import { useParams } from 'react-router';
+import { Redirect, useParams } from 'react-router';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+import Auth from '../utils/auth';
 
 import Card from '../components/Card';
 import Map from '../components/Map';
@@ -16,6 +18,12 @@ const Profile = () => {
     variables: { userId: userParam },
   });
   const userData = data?.me || data?.user || {};
+
+  console.log(userData);
+
+  if (Auth.loggedIn() && Auth.getProfile().data._id === userParam) {
+    return <Redirect to='/me' />
+  }
 
   return (
     <>
