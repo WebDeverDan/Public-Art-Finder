985020594c63b35af7737fdb16bac282ac57542a
Lauren Goss <laurenlgoss98@gmail.com>
2021-11-13 10:27:14 -0500

    added comments

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,9 @@
 import React from 'react';
+
+import Card from '../components/Card';
 import Map from '../components/Map';
 
+// TODO: Query all art for specific user, render to page
 const Profile = () => {
   return null;
 };
