53c52c3d7e60000ff27eeea8e73c0155286ebea0
Daniel Bradley <danielbvideo@gmail.com>
2021-11-15 20:40:12 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/Artin-Around",
+  process.env.MONGODB_URI || "mongodb://localhost/artin-around",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
