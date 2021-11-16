8f1d071b9bcb5da84948f12d686af36fb9228f7d
Daniel Bradley <danielbvideo@gmail.com>
2021-11-15 19:39:28 -0500

    deploying

--- client/src/components/ArtCard.js
+++ client/src/components/ArtCard.js
@@ -21,7 +21,7 @@ const ArtCard = ({ art }) => {
 
   return (
     <>
-      <Grid item key={card} xs={12} sm={6} md={4}>
+      <Grid item xs={12} sm={6} md={4}>
         <Card className={classes.card}>
           <CardMedia
             className={classes.cardMedia}
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/Public-Art-Finder",
+  process.env.MONGODB_URI || "mongodb://localhost/Pen-There",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
