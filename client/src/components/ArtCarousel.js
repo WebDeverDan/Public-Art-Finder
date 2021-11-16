0b2b2fa692bd1dceea672b23a6b18b36d8af843e
Daniel Bradley <danielbvideo@gmail.com>
2021-11-16 12:00:01 -0500

    updated model for art, and adjusted carousel styling

--- client/src/components/ArtCarousel.js
+++ client/src/components/ArtCarousel.js
@@ -6,8 +6,8 @@ function ArtCarousel({ art }) {
   const styles = {
     paper: {
       width: '100%',
-      height: '20%',
-      backgroundColor: 'gray',
+      height: '310px',
+      backgroundColor: 'white',
     },
   };
 
@@ -28,21 +28,38 @@ function ArtCarousel({ art }) {
   // ];
 
   return (
-    <Carousel>
+    <Carousel
+      indicatorIconButtonProps={{
+        style: {
+          color: '#9B752A',
+        },
+      }}
+      activeIndicatorIconButtonProps={{
+        style: {
+          color: 'white',
+        },
+      }}
+      navButtonsProps={{
+        style: {
+          backgroundColor: 'white',
+          borderRadius: 0,
+        },
+      }}
+    >
       {art.map((art, i) => (
         <Paper
           key={i}
           style={styles.paper}
           sx={{ pt: 0.5, flexDirection: 'column' }}
         >
-          <h2>{art.title}</h2>
+          {/* <h2>{art.title}</h2> */}
           <div className="carousel_image_box">
             <a href={`/art/${art._id}`}>
-            <img
-              className="carousel_image"
-              src={art.url}
-              alt={art.description}
-            />
+              <img
+                className="carousel_image"
+                src={art.url}
+                alt={art.description}
+              />
             </a>
           </div>
         </Paper>
